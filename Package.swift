d371fa79041ce478506e843ebcb4347b6a8be0fa
Thomas Rademaker <thomas@radmakr.com>
2024-07-13 17:06:00 -0400

    add CoreNostr dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,16 +6,19 @@ let package = Package(
     name: "swift-nostr-relay",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "swift-nostr-relay",
             targets: ["swift-nostr-relay"]),
     ],
+    dependencies: [
+        .package(url: "git@github.com:SparrowTek/CoreNostr.git", branch: "main"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-nostr-relay"),
+            name: "swift-nostr-relay",
+            dependencies: [
+                "CoreNostr"
+            ]),
         .testTarget(
             name: "swift-nostr-relayTests",
             dependencies: ["swift-nostr-relay"]
