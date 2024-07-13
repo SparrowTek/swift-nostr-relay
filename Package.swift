9dd42a35463f26f07c9cb56aea358636d4da6188
Thomas Rademaker <thomas@radmakr.com>
2024-07-13 16:46:37 -0400

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-nostr-relay",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "swift-nostr-relay",
+            targets: ["swift-nostr-relay"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "swift-nostr-relay"),
+        .testTarget(
+            name: "swift-nostr-relayTests",
+            dependencies: ["swift-nostr-relay"]
+        ),
+    ]
+)
